9eee05ade92c29b6816f0ce394c38505e82b41ab
xiaxiarui <2426129245@qq.com>
2026-09-14 21:27:16 +0800

    fix: 修正 baseUrl 和 npm install

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,8 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "zh-CN",
-    baseUrl: process.env.BASE_URL ?? "xiaxiarui.github.io",
+    baseUrl: process.env.BASE_URL ?? "https://xiaxiarui.github.io/Deep_Learning_Notes",
+,
     ignorePatterns: [
       "private",
       "xx-归档",
